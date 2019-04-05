7e8467f27c303aa833b6c662f11f43a64b358664
Marcus <mooi0002@student.monash.edu>
2019-04-06 08:57:31 +1100

    Update stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
-import MainTabNavigator from './MainTabNavigator';
+import Authenticated from './MainTabNavigator';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: Authenticated,
 }));
