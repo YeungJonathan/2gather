68bacc6924bb9fbed7d4cca3364ef2e7bf2f4cf6
zain610 <znshroff@gmail.com>
2019-04-06 16:33:20 +1100

    Completed adding drawer listitems. TODO add new screens recommended, starred, copyright, terms and conditions

--- screens/UserDetailsDrawer.js
+++ screens/UserDetailsDrawer.js
@@ -1,23 +1,86 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import Profile from "../components/Profile";
 import users from "../constants/FakeUserData";
-import { Divider } from "react-native-elements";
+import { Divider, ListItem, Button, Icon } from "react-native-elements";
+import { NavigationActions } from "react-navigation";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  ListItem: {
+    paddingLeft: 10
   }
 });
 
-const UserDetailsDrawer = () => (
+const UserDetailsDrawer = ({ navigation }) => (
   <ScrollView>
     <SafeAreaView
       style={styles.container}
       forceInset={{ top: "always", horizontal: "never" }}
     >
-      <Profile user={users[0]} />
-      <Divider style={{ backgroundColor: "blue" }} />;
+      <Profile
+        user={users[0]}
+        onPress={() => navigation.navigate("UserDetails")}
+      />
+      <Divider style={{ backgroundColor: "grey", height: 2 }} />
+      <View style={styles.ListItem}>
+        <TouchableOpacity onPress={() => navigation.navigate("Categories")}>
+          <ListItem
+            title="Home"
+            subtitle=""
+            leftAvatar={
+              <Icon reverse name="home" type="font-awesome" size={15} />
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Recommended")}>
+          <ListItem
+            title="Recommended"
+            subtitle=""
+            leftAvatar={
+              <Icon reverse name="thumbs-up" type="font-awesome" size={15} />
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Starred")}>
+          <ListItem
+            title="Starred"
+            subtitle=""
+            leftAvatar={
+              <Icon reverse name="star" type="font-awesome" size={15} />
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Terms and Conditions")}
+        >
+          <ListItem
+            title="Terms and Condition"
+            subtitle=""
+            leftAvatar={
+              <Icon reverse name="copyright" type="font-awesome" size={15} />
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => logout()}>
+          <ListItem
+            title="Logout"
+            subtitle=""
+            leftAvatar={
+              <Icon reverse name="sign-out-alt" type="font-awesome" size={15} />
+            }
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   </ScrollView>
 );
