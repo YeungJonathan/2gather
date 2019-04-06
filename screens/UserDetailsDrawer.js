4d916b09e7db095c979dc8bd9575b7ca0c3ce256
zain610 <znshroff@gmail.com>
2019-04-06 14:56:19 +1100

    added divider

--- screens/UserDetailsDrawer.js
+++ screens/UserDetailsDrawer.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
-import Profile from '../components/Profile';
-import users from '../constants/FakeUserData';
+import Profile from "../components/Profile";
+import users from "../constants/FakeUserData";
+import { Divider } from "react-native-elements";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 
-const UserDetailsDrawer = ({ navigation }) => (
-    <ScrollView>
-        <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
-        <Profile user={users[0]} onPress={() => navigation.navigate('UserDetails')} />
-        </SafeAreaView>
-    </ScrollView>
+const UserDetailsDrawer = () => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <Profile user={users[0]} />
+      <Divider style={{ backgroundColor: "blue" }} />;
+    </SafeAreaView>
+  </ScrollView>
 );
 
 export default UserDetailsDrawer;
