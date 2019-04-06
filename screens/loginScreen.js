fea757660353010f59cedca6b5df670d3738c74e
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-04-07 09:52:02 +1000

    logo

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, FlatList, Text, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
+import {View, FlatList, Text, TouchableOpacity, TextInput, ImageBackground, Image} from 'react-native';
 import {fakeEventItems} from '../constants/FakeEventItems';
 import { observer } from 'mobx-react';
 import EventCard from '../components/EventCard';
@@ -32,6 +32,10 @@ class loginScreen extends React.Component {
                 <View 
                     style={{flex: 1, justifyContent:'center', alignItems:'center'}}
                 >
+                    <Image
+                        source={require('../assets/images/logo.png')}
+                        style={{width: '50%',tintColor:'white'}}
+                    />
                     <View style={{flexDirection:'row', marginBottom:20}}>
                     <Text style={{fontSize: 18, color:'white'}}>
                             Login
@@ -59,7 +63,7 @@ class loginScreen extends React.Component {
                     </View>
                     <TouchableOpacity 
                         onPress={() => this.props.navigation.navigate("Categories", {name: this.state.userName})}
-                        style={{borderColor:'#3a85ff', borderWidth:1, borderRadius:10, marginTop: 20, backgroundColor:'#3a85ff', width: '50%', justifyContent:'center', alignItems:'center'}}    
+                        style={{borderColor:'#3a85ff', borderWidth:1, borderRadius:10, marginTop: 50, backgroundColor:'#3a85ff', width: '50%', justifyContent:'center', alignItems:'center'}}    
                     >
                         <Text style={{color: 'white', fontSize: 18, paddingBottom:10, paddingTop: 10}}>
                             Log In
