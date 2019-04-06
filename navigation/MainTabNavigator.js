58ce0ce42c79e481e42c0e51b7b1025b947b7d7d
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-04-06 15:24:22 +1100

    Event Details screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,11 +1,13 @@
 import { createStackNavigator } from 'react-navigation';
 import Categories from '../screens/Categories';
-import EventsList from '../screens/EventsList'
+import EventsList from '../screens/EventsList';
+import EventDetails from '../screens/EventDetails';
 
 const Authenticated = createStackNavigator(
     {
     Categories: {screen: Categories},
-    Events: {screen:EventsList}
+    Events: {screen:EventsList},
+    EventDetails: {screen:EventDetails}
     }, 
     {
         headerLayoutPreset: 'left'
