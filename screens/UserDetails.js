c00829efe03e91cc0b91899c08ac3714d4d4677d
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-04-06 22:35:31 +1100

    sstyling

--- screens/UserDetails.js
+++ screens/UserDetails.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { ScrollView, View, TouchableOpacity } from "react-native";
+import { ScrollView, View, TouchableOpacity, FlatList } from "react-native";
 import Profile from "../components/Profile";
-import Interests from "../components/Interests";
+// import Interests from "../components/Interests";
 import UserDescription from "../components/UserDescription";
 import { ListItem, Card } from "react-native-elements";
 import appStore from "../stores/AppStore";
+import InterestItem from '../components/InterestItem';
+
 
 export default class UserDetails extends React.Component {
   static navigationOptions = () => {
@@ -13,11 +15,22 @@ export default class UserDetails extends React.Component {
     };
   };
   render() {
+    const interests = appStore.users[0].interests;
     return (
       <ScrollView style={{ flex: 1, height: "100%" }}>
         <Profile user={appStore.users[0]} disabled />
         <UserDescription description={appStore.users[0].description} />
-        <Interests user={appStore.users[0]} />
+        <Card title="Interests">
+          <FlatList
+              data={interests}
+              keyExtractor = {(item, index) => index.toString()}
+              renderItem={item => 
+                  <InterestItem
+                      interest={item.item}                            
+                  />
+                  }
+            />
+        </Card>
         <Card title="History">
           {appStore.users[0].previous.map(eventId => (
             <TouchableOpacity
