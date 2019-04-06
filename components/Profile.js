cfad0bdaaaf055265e6ec13455f9efb011c9d7e3
zain610 <znshroff@gmail.com>
2019-04-06 14:44:50 +1100

    Made Description component, added to UserDetails

--- components/Profile.js
+++ components/Profile.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Text, Image, View } from "react-native";
-import {
-  Card,
-  ListItem,
-} from "react-native-elements";
+import { Card, ListItem } from "react-native-elements";
 
 const Profile = ({ user }) => (
   <View>
@@ -22,20 +19,6 @@ const Profile = ({ user }) => (
       title={user.name}
       subtitle={user.role}
     />
-
-    <Card title="Description">
-      <Text>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not
-        only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s
-        with the release of Letraset sheets containing Lorem Ipsum passages,
-        and more recently with desktop publishing software like Aldus
-        PageMaker including versions of Lorem Ipsum
-      </Text>
-    </Card>
   </View>
 );
 
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -2,6 +2,7 @@ import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 import Categories from "../screens/Categories";
 import UserDetails from "../screens/UserDetails";
 import UserDetailsDrawer from "../screens/UserDetailsDrawer";
+import EventsList from "../screens/EventsList";
 
 const Authenticated = createDrawerNavigator(
   {
