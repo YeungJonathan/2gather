b5bfb625832270c8107a2d1064729aa6756df0e5
MarcusKJOoi <38354039+MarcusKJOoi@users.noreply.github.com>
2019-04-07 03:30:13 +1000

    Overlay to add units (#18)

--- screens/UserDetails.js
+++ screens/UserDetails.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { ScrollView, View, TouchableOpacity, FlatList } from "react-native";
+import { ScrollView, View, TextInput, Text, TouchableOpacity, FlatList } from "react-native";
+import {toJS} from 'mobx';
+import {observer} from 'mobx-react';
 import Profile from "../components/Profile";
-// import Interests from "../components/Interests";
 import UserDescription from "../components/UserDescription";
-import { ListItem, Card } from "react-native-elements";
+import { ListItem, Card, Overlay } from "react-native-elements";
 import appStore from "../stores/AppStore";
 import InterestItem from '../components/InterestItem';
-
 import UnitsList from "../components/UnitsList";
 
+@observer
 export default class UserDetails extends React.Component {
   static navigationOptions = () => {
     return {
       header: null
     };
   };
+
+  constructor() {
+    super();
+    this.state = {
+      overlayVisible: false,
+      addingUnitCode: ''
+    }
+  }
+
   render() {
-    const interests = appStore.users[0].interests;
+    const interests = appStore.users[0].interests,
+      {overlayVisible, addingUnitCode} = this.state;
     return (
       <ScrollView style={{ flex: 1, height: "100%" }}>
         <Profile user={appStore.users[0]} disabled />
@@ -50,8 +61,44 @@ export default class UserDetails extends React.Component {
           ))}
         </Card>
         <Card title="Units">
-          <UnitsList units={appStore.users[0].units} />
+          <TouchableOpacity
+            onPress={() => this.setState({overlayVisible: true})}
+            style={{justifyContent: 'center', alignSelf: 'center', paddingBottom: 15}}
+          >
+            <Text>Add a unit</Text>
+          </TouchableOpacity>
+          <UnitsList units={toJS(appStore.users[0].units)} />
         </Card>
+        <Overlay 
+          isVisible={overlayVisible}
+          height={75}
+        >
+          <View style={{flex: 1}}>
+            <TextInput 
+              placeholder='Enter your unit code here'
+              value={addingUnitCode}
+              onChangeText={(text) => this.setState({addingUnitCode: text})}
+              autoCapitalize='characters'
+            />
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({overlayVisible: false, addingUnitCode: ''});
+                }}
+              >
+                <Text>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  if(addingUnitCode !== '') appStore.users[0].units.push(addingUnitCode);
+                  this.setState({overlayVisible: false, addingUnitCode: ''});
+                }}
+              >
+                <Text>Submit</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Overlay>
       </ScrollView>
     );
   }
